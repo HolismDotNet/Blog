5677d323696434651edbbd7ebee2514a3ded28ea
“Sanam” <pourmand2010@users.noreply.github.com>
2021-12-16 12:50:59 +0330

    committed by script

---
+++ Business/PostBusiness.cs
@@ -0,0 +1,9 @@
+﻿namespace Holism.Blog.Business;
+{
+    public class PostBusiness : Business<Post, Post>
+    {
+        protected override Repository<Post> WriteRepository => Repository.Post;
+
+        protected override ReadRepository<Post> ReadRepository => Repository.Post;
+    }
+}
