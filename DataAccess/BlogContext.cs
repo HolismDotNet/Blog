a2ad583002adc6e334c64b146165b60ba26eedad
Saeed Nemati <sn.1361@gmail.com>
2022-01-25 10:13:49 +0330

    committed by script

--- DataAccess/BlogContext.cs
+++ DataAccess/BlogContext.cs
@@ -4,9 +4,9 @@ public class BlogContext : DatabaseContext
 {
     public override string ConnectionStringName => "Blog";
 
-    public DbSet<PostContent> PostContents { get; set; }
+    public DbSet<Blog.PostContent> PostContents { get; set; }
 
-    public DbSet<Post> Posts { get; set; }
+    public DbSet<Blog.Post> Posts { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
