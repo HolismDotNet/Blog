be4eab6d82a05fc9834149c86c4b240d0be6c48e
Saeed Nemati <sn.1361@gmail.com>
2022-04-04 17:02:14 +0430

    committed by script

--- Business/PostContentBusiness.cs
+++ Business/PostContentBusiness.cs
@@ -5,4 +5,17 @@ public class PostContentBusiness : Business<Blog.PostContent, Blog.PostContent>
     protected override Read<Blog.PostContent> Read => Repository.PostContent;
 
     protected override Write<Blog.PostContent> Write => Repository.PostContent;
+
+    public override Blog.PostContent Get(long id)
+    {
+        var postContent = Read.Get(id);
+        if (postContent == null)
+        {
+            postContent = new PostContent();
+            postContent.Id = id;
+            postContent.Content = "[]";
+            Create(postContent);
+        }
+        return postContent;
+    }
 }
