16539aa9bc61e4a1441fd5a8394d90b0437f1631
Saeed Nemati <sn.1361@gmail.com>
2022-05-23 09:02:29 +0430

    committed by script

--- Business/PostContentBusiness.cs
+++ Business/PostContentBusiness.cs
@@ -2,6 +2,8 @@ namespace Blog;
 
 public class PostContentBusiness : Business<Blog.PostContent, Blog.PostContent>
 {
+    public override string EntityType => "BlogPostContent";
+
     protected override Read<Blog.PostContent> Read => Repository.PostContent;
 
     protected override Write<Blog.PostContent> Write => Repository.PostContent;
