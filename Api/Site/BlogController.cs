0684c4d9d9e6111152ebd02d2015321d88b76185
Saeed Nemati <sn.1361@gmail.com>
2022-05-24 13:52:29 +0430

    committed by script

--- Api/Site/BlogController.cs
+++ Api/Site/BlogController.cs
@@ -3,21 +3,58 @@ namespace Blog;
 public class BlogController : HolismController
 {
     [BindProperty(SupportsGet=true)]
-    public int? PostsCount { get; set; }
+    public int? PageSize { get; set; }
+
+    [BindProperty(SupportsGet=true)]
+    public int? PageNumber { get; set; }
+
+    [BindProperty(SupportsGet=true)]
+    public string Category { get; set; }
+
+    [BindProperty(SupportsGet=true)]
+    public string Tag { get; set; }
 
     [HttpGet]
     public object Data()
     {
         dynamic data = new ExpandoObject();
-        var postsListParameters = ListParameters.Create();
-        if (PostsCount.HasValue)
-        {
-            postsListParameters.PageSize = PostsCount.Value;
-        }
-        data.Posts = new Blog.PostBusiness().GetList(postsListParameters);
-        data.Categories = new HierarchyBusiness().GetTree(new Blog.PostBusiness().EntityType);
-        data.Tags = new TagBusiness().GetTags(new Blog.PostBusiness().EntityType);
-        data.Seo = new ParametersObject();
+        data.Posts = GetPosts();
+        data.Categories = GetCategories();
+        data.Tags = GetTags();
+        data.Seo = GetSeo();
         return data;
+    }
+
+    public ListResult<Blog.PostView> GetPosts()
+    {
+        var postsListParameters = ListParameters.Create();
+        if (PageSize.HasValue)
+        {
+            postsListParameters.PageSize = PageSize.Value;
+        }
+        if (PageNumber.HasValue)
+        {
+            postsListParameters.PageNumber = PageNumber.Value;
+        }
+        var posts = new Blog.PostBusiness().GetList(postsListParameters);
+        return posts;
+    }
+
+    public List<HierarchyView> GetCategories()
+    {
+        var categories = new HierarchyBusiness().GetTree(new Blog.PostBusiness().EntityType);
+        return categories;
+    }
+
+    public List<Tag> GetTags()
+    {
+        var tags = new TagBusiness().GetTags(new Blog.PostBusiness().EntityType);
+        return tags;
+    }
+
+    public object GetSeo()
+    {
+        var seo = new ParametersObject();
+        return seo;
     }
 }
