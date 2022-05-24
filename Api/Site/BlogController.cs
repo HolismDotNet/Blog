c563c7037fe096271aa4b81de7575b96f369a7a9
Saeed Nemati <sn.1361@gmail.com>
2022-05-24 08:38:23 +0430

    committed by script

--- Api/Site/BlogController.cs
+++ Api/Site/BlogController.cs
@@ -7,6 +7,9 @@ public class BlogController : HolismController
     {
         dynamic data = new ExpandoObject();
         data.Posts = new Blog.PostBusiness().GetList(ListParameters.Create());
+        data.Categories = new HierarchyBusiness().GetTree(new Blog.PostBusiness().EntityType);
+        data.Tags = new TagBusiness().GetTags(new Blog.PostBusiness().EntityType);
+        data.Seo = new ParametersObject();
         return data;
     }
 }
