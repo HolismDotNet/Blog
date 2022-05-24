3016afc43ede00bc158720631f539913d353b19c
Saeed Nemati <sn.1361@gmail.com>
2022-05-24 10:16:29 +0430

    committed by script

--- Api/Site/BlogController.cs
+++ Api/Site/BlogController.cs
@@ -2,11 +2,19 @@ namespace Blog;
 
 public class BlogController : HolismController
 {
+    [BindProperty(SupportsGet=true)]
+    public int? PostsCount { get; set; }
+
     [HttpGet]
     public object Data()
     {
         dynamic data = new ExpandoObject();
-        data.Posts = new Blog.PostBusiness().GetList(ListParameters.Create());
+        var postsListParameters = ListParameters.Create();
+        if (PostsCount.HasValue)
+        {
+            postsListParameters.PageSize = PostsCount.Value;
+        }
+        data.Posts = new Blog.PostBusiness().GetList(postsListParameters);
         data.Categories = new HierarchyBusiness().GetTree(new Blog.PostBusiness().EntityType);
         data.Tags = new TagBusiness().GetTags(new Blog.PostBusiness().EntityType);
         data.Seo = new ParametersObject();
