d314f45eda1765e8ae37914b1f4907ac292d8aab
Saeed Nemati <sn.1361@gmail.com>
2022-05-25 09:20:55 +0430

    committed by script

--- Api/Site/BlogController.cs
+++ Api/Site/BlogController.cs
@@ -11,8 +11,12 @@ public class BlogController : HolismController
     [BindProperty(SupportsGet=true)]
     public string Hierarchy { get; set; }
 
+    public HierarchyView DatabaseHierarchy { get; set; }
+
     [BindProperty(SupportsGet=true)]
     public string Tag { get; set; }
+
+    public Tag DatabaseTag { get; set; }
 
     public string CanonicalUrl { get; set; }
 
@@ -45,19 +49,60 @@ public class BlogController : HolismController
     public List<HierarchyView> GetHierarchies()
     {
         var hierarchies = new HierarchyBusiness().GetTree(new Blog.PostBusiness().EntityType);
+        if (Hierarchy.IsSomething())
+        {
+            DatabaseHierarchy = new HierarchyBusiness().GetBySlug(Hierarchy);
+        }
         return hierarchies;
     }
 
     public List<Tag> GetTags()
     {
         var tags = new TagBusiness().GetTags(new Blog.PostBusiness().EntityType);
+        if (Tag.IsSomething())
+        {
+            DatabaseTag = new TagBusiness().GetBySlug(Tag);
+        }
         return tags;
     }
 
     public object GetSeo()
     {
-        var seo = new ParametersObject();
+        var seo = new ParametersObject();//new PathParameterBusiness().Get(Request.GetEncodedPathAndQuery());
         CanonicalUrl = "/blog";
+        if (seo != null)
+        {
+            return seo;
+        }
+        if (DatabaseHierarchy != null)
+        {
+            seo = new EntityParameterBusiness().Get(new HierarchyBusiness().EntityType, DatabaseHierarchy.Guid).CastTo<ParametersObject>();
+            if (seo == null)
+            {
+                seo = new ParametersObject();
+                seo.PageTitle = DatabaseHierarchy.Title;
+                seo.PageDescription = DatabaseHierarchy.Description;
+            }
+        }
+        else if (DatabaseTag != null)
+        {
+            seo = new EntityParameterBusiness().Get(new TagBusiness().EntityType, DatabaseTag.Guid).CastTo<ParametersObject>();
+            if (seo == null)
+            {
+                seo = new ParametersObject();
+                seo.PageTitle = DatabaseTag.Name;
+                seo.PageDescription = DatabaseTag.Description;
+            }
+        }
+        if (seo == null)
+        {
+            seo = new QueryParameterBusiness().Get(new QueryParameterBusiness().GetSortedQuery(Request.Query)).CastTo<ParametersObject>();
+        }
+        if (seo == null)
+        {
+            seo = new ParametersObject();
+            seo.PageTitle = "Blog";
+        }
         return seo;
     }
 }
