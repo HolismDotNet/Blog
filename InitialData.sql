e853391047a8bfe0b24da61776233b165f5b592f
Saeed Nemati <sn.1361@gmail.com>
2022-07-02 18:33:50 +0430

    committed by script

--- InitialData.sql
+++
@@ -1,4 +0,0 @@
-insert ignore into Blog.Posts(Guid, Slug, Title, Summary, TimeToRead, ImageGuid, AcceptsComment, UtcDate, StateId) values
-(uuid(), 'how-to-grow', 'How to grow', 'We all want to become better. Let us see how can we achieve this goal.', 3, null, 1, now(), 1),
-(uuid(), 'most-wanted-skills', 'Most wanted skills', 'The list of skills that you can learn and rest assured that you can make money from.', 2, null, 0, now(), 2),
-(uuid(), '5-ways-to-stay-healthy', '5 ways to stay healthy', 'If you want to change your lifestyle for better health, read these 5 tips.', 5, null, 1, now(), 1);
